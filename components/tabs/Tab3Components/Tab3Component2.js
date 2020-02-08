23578c7881ce66a27e497e830e6eb1e5dca90dd5
Ganesh Bhusi <ganeshpaul999@gmail.com>
2020-02-08 13:42:18 +0530

    modified

--- components/tabs/Tab3Components/Tab3Component2.js
+++ components/tabs/Tab3Components/Tab3Component2.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import { styles } from '../../styles';
+import { StackActions, NavigationActions } from 'react-navigation';
 
 function Tab3Component2(props) {
     const {navigate} = props.navigation;
+    const action = StackActions.reset({
+        index: 0,
+        actions: [
+            NavigationActions.navigate({routeName: "Home"})
+        ]
+    })
     return (
         <View>
             <TouchableOpacity
             onPress={()=>{
                 console.log(navigate)
-                navigate("TabOne")
+                props.navigation.dispatch(action)
             }}
             >
                 <View style={styles.container}>
